5904da7fcb1fc08ff1de78fa2922b4904c7010d3
Jack Leigh <jack.leigh@gmail.com>
2017-12-02 19:02:27 +0000

    Day 1, part 2

--- test/Main.purs
+++ test/Main.purs
@@ -7,7 +7,7 @@ import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (RunnerEffects, run)
 
-import Main (captcha)
+import Main (captcha, captcha2)
 
 main :: Eff (RunnerEffects ()) Unit
 main = run [consoleReporter] do
@@ -16,3 +16,9 @@ main = run [consoleReporter] do
     it "1111" $ captcha "1111" `shouldEqual` 4
     it "1234" $ captcha "1234" `shouldEqual` 0
     it "91212129" $ captcha "91212129" `shouldEqual` 9
+  describe "day 1 part 2" do
+    it "1212" $ captcha2 "1212" `shouldEqual` 6
+    it "1221" $ captcha2 "1221" `shouldEqual` 0
+    it "123425" $ captcha2 "123425" `shouldEqual` 4
+    it "123123" $ captcha2 "123123" `shouldEqual` 12
+    it "12131415" $ captcha2 "12131415" `shouldEqual` 4
